68a6532899588bcf62098d81f07c97e336c4c54a
itgsod-carl-alexandersson <carl.alexandersson@elev.it-gymnasiet.se>
2015-09-15 12:06:01 +0200

    Fixade tillbaka kommentarer

--- tickets/tickets.py
+++ tickets/tickets.py
@@ -1,10 +1,8 @@
-"""
-coding=utf-8
-Funktionen tickets kollar variabeln age och beroende på dess värde skriver funktionen
-ut hur mycket användaren ska betala i SEK.
-Om man skriver in något annat än positiva tal så kommer funktionen skriva ut vad man gjorde fel och sedan
-starta om sig själv
-"""
+# coding=utf-8
+# Funktionen tickets kollar variabeln age och beroende på dess värde skriver funktionen
+# ut hur mycket användaren ska betala i SEK.
+# Om man skriver in något annat än positiva tal så kommer funktionen skriva ut vad man gjorde fel och sedan
+# starta om sig själv.
 
 def tickets(age):
     if age.isdigit() is True:
--- tickettest.py
+++ tickettest.py
@@ -1,4 +1,5 @@
 from tickets.tickets import tickets
 
+print "Input a positive number"
 age = raw_input()
 tickets(age)
